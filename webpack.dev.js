b265060bfceb492dee76fcf9186cbe2b5466d8ce
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-25 09:23:29 +0300

    all webpack packs and plugins installed, tested, works

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,23 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //html generavimo pluginas.
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     plugins: [
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                plugins: [
+                    ['imagemin-webp'],
+                    ['mozjpeg', { quality: 50 }],
+                    ['gifsicle'],
+                    ['pngquant'],
+                    ['imagemin-svgo'],
+                ],
+            },
+        }),
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: './src/html/template.html',
             templateParameters: {
@@ -17,5 +31,33 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        assetModuleFilename: 'images/[name][ext]',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 5000,
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, //taspats ieskosim js failu, isskyrus(exlude) node_modules faile
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'], //kiek daug atgal i praeiti mes norim sugrizti(kad veiktu ant senu narsykliu)
+                    },
+                },
+            },
+        ],
     },
 };
